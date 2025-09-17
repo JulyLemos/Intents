9530d295ad9bb34052106410b3f983f4926f0c1c
July <julylemos1@hotmail.com>
2025-09-16 21:05:18 -0300

    Função de abrir url no navegador

--- app/src/main/java/br/edu/scl/ifsp/sdm/intents/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/intents/MainActivity.kt
@@ -1,15 +1,27 @@
 package br.edu.scl.ifsp.sdm.intents
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.Intent.ACTION_VIEW
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
+import br.edu.scl.ifsp.sdm.intents.Extras.PARAMETER_EXTRA
 import br.edu.scl.ifsp.sdm.intents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val PARAMETER_REQUEST_CODE = 0
+    }
+
     private val activityMainBinding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private lateinit var parameterArl: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +29,31 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(activityMainBinding.toolbarIn.toolbar)
         supportActionBar?.subtitle = localClassName
 
-        activityMainBinding.parameterBt.setOnClickListener {
+        parameterArl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result -> //isola o tratamento do retorno do fechamento de cada tela secundária
+            if (result.resultCode == RESULT_OK) {
+                result.data?.getStringExtra(PARAMETER_EXTRA)?.also {
+                    activityMainBinding.parameterTv.text = it
+                }
+            }
+        }
 
+        activityMainBinding.apply {
+            parameterBt.setOnClickListener { //provoca a abertura da tela através da intent
+                val parameterIntent = Intent(this@MainActivity, ParameterActivity::class.java).apply {
+                    putExtra(PARAMETER_EXTRA, parameterTv.text)
+                }
+                parameterArl.launch(parameterIntent)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PARAMETER_REQUEST_CODE && resultCode == RESULT_OK) {
+            data?.getStringExtra(PARAMETER_EXTRA)?.also {
+                activityMainBinding.parameterTv.text = it
+            }
         }
     }
 
@@ -30,9 +65,19 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.openActivityMi -> {
+                val parameterIntent = Intent("OPEN_PARAMETER_ACTIVITY_ACTION").apply {
+                    putExtra(PARAMETER_EXTRA, activityMainBinding.parameterTv.text)
+                }
+                parameterArl.launch(parameterIntent)
                 true
             }
             R.id.viewMi -> {
+                //urls estão dentro de uri
+                //na vídeo aula é: val url = Uri.parse(activityMainBinding.parameterTv.text.toString(()
+                //porém o compilador não recoheceu e mostrou outra forma de escrita
+                val url = activityMainBinding.parameterTv.text.toString().toUri()
+                val browserIntent = Intent(ACTION_VIEW, url)
+                startActivity(browserIntent)
                 true
             }
 
--- app/src/main/java/br/edu/scl/ifsp/sdm/intents/ParameterActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/intents/ParameterActivity.kt
@@ -1,8 +1,11 @@
 package br.edu.scl.ifsp.sdm.intents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.edu.scl.ifsp.sdm.intents.Extras.PARAMETER_EXTRA
 import br.edu.scl.ifsp.sdm.intents.databinding.ActivityParameterBinding
+
 
 class ParameterActivity : AppCompatActivity() {
     private val activityParameterBinding: ActivityParameterBinding by lazy {
@@ -14,8 +17,19 @@ class ParameterActivity : AppCompatActivity() {
         setSupportActionBar(activityParameterBinding.toolbarIn.toolbar)
         supportActionBar?.subtitle = localClassName
 
-        activityParameterBinding.returnCloseBt.setOnClickListener{
+        intent.getStringExtra(PARAMETER_EXTRA)?.let {
+            activityParameterBinding.parameterEt.setText(it) //aquilo que foi enviado pela mainactivity, seja recebido pela parameteractivity
+        }
 
+        activityParameterBinding.apply {
+            returnCloseBt.setOnClickListener {// criar uma intent vazia para retornar
+                val resultIntent = Intent().apply {
+                    putExtra(PARAMETER_EXTRA, parameterEt.text.toString())
+                }
+                //result é definida em activity
+                setResult(RESULT_OK, resultIntent)
+                finish()
+            }
         }
     }
 }
